8c1f3e851cfa11d3a081e17fdb07eed5eeb24847
matthews <cnusrinivasu@gmail.com>
2025-09-12 13:35:22 +0530

    sum of 2 arguments

--- 06-array.sh
+++ 06-array.sh
@@ -6,4 +6,4 @@ FRUITS=("APPLE" "ORANGE" "MANGO")
 echo " First is :${FRUITS[1]}"
 echo " Second is :${FRUITS[0]}"
 echo " Third is : ${FRUITS[2]}"
-echo " All : ${FRUITS[@]}"
+echo " All : ${FRUITS[@]}" # @ is for all
---
+++ 07-addition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Addition by using Variables with arg
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1 + NUMBER2))
+
+echo "The sum of the numbers are: $SUM"
+
