934efceaa0661048e0bf2669d784d00119c9b3f5
matthews <cnusrinivasu@gmail.com>
2025-09-12 11:58:59 +0530

    2nd arg

---
+++ 05-read-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your name"
+read NAME
+echo "Enter your place"
+read PLACE
+echo "Enter your sex"
+read AGE
+echo "Your name is : $NAME"
+echo "Your place is : $PLACE"
+echo "Your age is : $AGE"
