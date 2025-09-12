3ba3b7f36519d11061050b9e55abfdeade4fc6f8
matthews <cnusrinivasu@gmail.com>
2025-09-12 14:01:00 +0530

    To Find proccessing ID executing  207

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,4 +4,5 @@ echo "All variables passed to the script: $@" # All variabl passed through @
 echo " Number of variables passed : $#" # To know that whatever variables passed
 echo " Found the script name: $0" # To know what the script name is
 echo " current working directory $PWD" # To know current directory but here mentioned capital PWD
-echo " Home director of user $HOME" # to know current directory of the user home directory.
+echo " Home director of user $HOME" # To know current directory of the user home directory.
+echo " PID of the script executing now: $$" To know Proccessing ID
