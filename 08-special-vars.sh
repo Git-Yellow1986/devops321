388efb861909783f109c9f994e79888590e5b974
matthews <cnusrinivasu@gmail.com>
2025-09-12 13:50:40 +0530

    to know current directory by

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,4 +3,4 @@
 echo "All variables passed to the script: $@" # All variabl passed through @
 echo " Number of variables passed : $#" # To know that whatever variables passed
 echo " Found the script name: $0" # To know what the script name is
-
+echo " current working directory $pwd" # To know current directory
