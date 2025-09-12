a5fe6482f876c1fe86c11ae1d1d66d7b45686813
matthews <cnusrinivasu@gmail.com>
2025-09-12 14:39:50 +0530

    Condition sample with gt and lt

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $UMBER -gt ] # gt,lt,ne,eq, ge etc...
+then 
+    echo "Given number is :$NUMBER is grater than 20"
+else
+    echo "Give number is :$NUMBER is less than 20"
+First
