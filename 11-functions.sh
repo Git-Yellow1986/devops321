087495bfddcb3845918631e1389387fe2c5f609d
matthews <cnusrinivasu@gmail.com>
2025-09-12 17:05:33 +0530

    VALIDATION

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+USERID=$(id -u) # id -u is give the root id 
+# echo "User ID is : $USERID"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is .....FAILED"
+        exit 1
+    else
+        echo "$2 is ......SUCCESS"
+    fi
+
+}
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this scrip root privelege"
+    exit 1
+fi
+
+ dnf list installed git
+
+VALIDATE $? "Listing git"
+
+ 
