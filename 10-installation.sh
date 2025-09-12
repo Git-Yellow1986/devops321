9506b79b3696a2542be8649a68d458397261a871
matthews <cnusrinivasu@gmail.com>
2025-09-12 15:50:37 +0530

    run this script in root priveleges

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,6 +1,9 @@
-USERID=$(id -u)
+USERID=$(id -u) # id -u is give the root id 
 if [ $USERID -ne 0 ]
 then
     echo "Please run this scrip root privelege"
     exit 1
 fi
+
+dnf list installed git
+
