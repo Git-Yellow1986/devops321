6b893ba46a0dee4072c8bc7812e07ee5d239d147
matthews <cnusrinivasu@gmail.com>
2025-09-15 12:32:37 +0530

    redirection on both sides

--- 17-redir-terminal.sh
+++ 17-redir-terminal.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# Note: Regarding LOGS - both Terminal and log_files
 LOGS_FOLDER="/var/log/shell-script"
 #echo redirector.sh | cut -d "." -f1 --> on command prompt
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$CRIPT_NAME-$TIME_STAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOGS_FOLDER
+
+
+
+
+# # Note: Regarding LOGS - both Terminal and log_files
+# LOGS_FOLDER="/var/log/shell-script"
+# #echo redirector.sh | cut -d "." -f1 --> on command prompt
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE="$LOGS_FOLDER/$CRIPT_NAME-$TIME_STAMP.log"
+# mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m" # color red
@@ -47,18 +57,6 @@ then
 fi
 
 # sh 15-loops.sh git mysql nginx
-# for package in $@ # $@ refers to all arguments passed to it
-# do
-#     dnf list installed $package | tee -a $LOG_FILE
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "$package is not installed, goint to install it...."
-#         dnf install $package -y | tee -a $LOG_FILE
-#         VALIDATE $? "installing $package"
-#     else
-#         echo -e "$package is aleady $Y installed..nothing to do $N"
-#     fi
-# done
 
 for package in $@ # $@ refers to all arguments passed to it
 do
